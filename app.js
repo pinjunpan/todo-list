4bdfdd2d415b5c51a892c9fb344e4a23020bcacb
pinjunpan <pinjunpann@gmail.com>
2024-10-02 16:05:24 +0800

    router init

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req,res) => {
-  res.send('Hello World')
+app.get('/todos', (req, res) => {
+  res.send('get all todos')
 })
 
-app.listen(3000, (req,res) => {
+app.get('/todos/new', (req, res) => {
+  res.send('create todo');
+})
+
+app.get('/todos/:id', (req, res) => {
+  res.send(`get todo: ${req.params.id}`)
+})
+
+app.post('/todos', (req, res) => {
+  res.send('add todo')
+})
+
+app.get('/todos/:id/edit', (req, res) => {
+  res.send(`get todo edit: ${req.params.id}`)
+})
+
+app.put('/todos/:id', (req, res) => {
+  res.send('modify todo')
+})
+
+app.delete('/todos/:id', (req, res) => {
+  res.send('delete todo')
+})
+
+app.listen(3000, (req, res) => {
   console.log('App is running on port 3000')
 })
