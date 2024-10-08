e29cebf1822b83ef7244b3f9a8eebda0f01ca32a
pinjunpan <pinjunpann@gmail.com>
2024-10-08 17:32:38 +0800

    implement updating todo

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 
 const { engine } = require('express-handlebars')
+const methodOverride = require('method-override')
 
 const db = require('./models')
 const Todo = db.Todo
@@ -12,6 +13,7 @@ app.set('view engine', '.hbs')
 app.set('views', './views')
 
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
 	res.render('index')
@@ -50,11 +52,23 @@ app.get('/todos/:id', (req, res) => {
 })
 
 app.get('/todos/:id/edit', (req, res) => {
-  res.send(`get todo edit: ${req.params.id}`)
+  const id = req.params.id
+
+  return Todo.findByPk(id, {
+    attributes: ['id', 'name'],
+    raw: true
+  })
+          .then((todo) => res.render('edit', {todo}))
+          .catch((err) => console.log(err))
 })
 
 app.put('/todos/:id', (req, res) => {
-  res.send('modify todo')
+  const body = req.body
+  const id = req.params.id
+
+  return Todo.update({name: body.name}, {where: {id}})
+          .then(() => res.redirect(`/todos/${id}`))
+
 })
 
 app.delete('/todos/:id', (req, res) => {
