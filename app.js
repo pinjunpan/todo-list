93f050f30eb8597ca91964f400a88b2d6114df82
pinjunpan <pinjunpann@gmail.com>
2024-10-08 18:38:03 +0800

    implement deleting todo

--- app.js
+++ app.js
@@ -72,7 +72,10 @@ app.put('/todos/:id', (req, res) => {
 })
 
 app.delete('/todos/:id', (req, res) => {
-  res.send('delete todo')
+  const id = req.params.id
+
+  return Todo.destroy({where: {id}})
+          .then(() => res.redirect('/todos'))
 })
 
 app.listen(port, () => {
