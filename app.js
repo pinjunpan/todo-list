31ddae74f1e02cbb6eeb192940df810524472a0b
pinjunpan <pinjunpann@gmail.com>
2024-10-16 15:57:54 +0800

    add flash messages

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 const port = 3000
 
@@ -15,6 +17,13 @@ app.set('views', './views')
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 
+app.use(session({
+  secret: 'ThisIsSecret',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
+
 app.get('/', (req, res) => {
 	res.redirect('/todos')
 })
@@ -24,7 +33,7 @@ app.get('/todos', (req, res) => {
     attributes: ['id', 'name', 'isComplete'],
     raw: true
   })
-		.then((todos) => res.render('todos', { todos }))
+		.then((todos) => res.render('todos', { todos, message: req.flash('success') }))
 		.catch((err) => res.status(422).json(err))
 })
 
@@ -36,7 +45,10 @@ app.post('/todos', (req, res) => {
   const name = req.body.name
 
   return Todo.create({name})
-    .then(() => res.redirect('/todos'))
+    .then(() => {
+      req.flash('success', '新增成功！')
+      return res.redirect('/todos')
+    })
     .catch((err) => console.log(err))
 })
 
@@ -47,7 +59,7 @@ app.get('/todos/:id', (req, res) => {
     attributes: ['id', 'name', 'isComplete'],
     raw: true
   })
-    .then((todo) => res.render('todo', {todo}))
+    .then((todo) => res.render('todo', {todo, message: req.flash('success')}))
     .catch((err) => console.log(err))
 })
 
@@ -67,14 +79,20 @@ app.put('/todos/:id', (req, res) => {
   const id = req.params.id
 
   return Todo.update({name, isComplete: isComplete === 'completed'}, {where: {id}})
-          .then(() => res.redirect(`/todos/${id}`))
+          .then(() => {
+            req.flash('success', '編輯成功！')
+            return res.redirect(`/todos/${id}`)
+          })
 })
 
 app.delete('/todos/:id', (req, res) => {
   const id = req.params.id
 
   return Todo.destroy({where: {id}})
-          .then(() => res.redirect('/todos'))
+          .then(() => {
+            req.flash('success', '刪除成功！')
+            res.redirect('/todos')
+          })
 })
 
 app.listen(port, () => {
